dc26ddd09986ebfab9823e00a74284cfe596dc39
unknown <taewoo1212@gmail>
2024-02-12 20:33:07 +0900

    리액트 week-4 과제

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,16 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+import reactJsxRuntime from 'react/jsx-runtime';
 export default defineConfig({
   // 배포 경로 설정
   base: '/',
   plugins: [react(), checker({ typescript: true })],
   // 빌드 설정
   build: {
+    rollupOptions: {
+      external: [reactJsxRuntime],
+    },
     outDir: 'dist', // 빌드 결과물이 생성될 폴더 설정
     emptyOutDir: true, // 빌드 전에 outDir 폴더를 비우는지 여부 설정
     sourcemap: true, // 소스맵 생성 여부 설정
